f849c79267353c43bdbcbba65c7e5af81befe728
JohnRSinx <johnrodrigues97106@gmail.com>
2025-03-14 10:20:17 -0300

    feat: add comentarios para explicacao

--- src/store/cart/reducer.ts
+++ src/store/cart/reducer.ts
@@ -12,7 +12,21 @@ export const cartReducer = (state = initialState, action: { type: string }) => {
         ...state,
         countCart: state.countCart + 1,
       };
+    
     default:
       return state;
   }
 };
+
+// Reducers => é uma função com 2 parametros State - Action 
+//Os Reducers são funções que recebem o estado atual e uma ação, e retornam um novo estado.
+//  Eles são responsáveis por atualizar o estado da aplicação com base nas ações que ocorrem.
+
+// State = initialState
+
+//Action => type, 
+
+//Dispatch e useSelector 
+
+//Dispatch 
+
